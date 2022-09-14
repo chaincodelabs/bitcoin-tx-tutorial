5744adec86d3f242eb6d1cba204ff50210df56d7
Darius Parvin <darius@berkeley.edu>
2022-09-14 09:45:00 -0700

    refactor setup function

--- functions/create_regtest_utxo.py
+++ functions/create_regtest_utxo.py
@@ -31,11 +31,7 @@ def setup_regtest_bitcoind() -> str:
     blockcount = subprocess.getoutput(f"{CLI} getblockcount")
     assert(blockcount == "101")
 
-    return mining_address
-
-def fund_address(address: str, amount: float) -> str:
-    txid = subprocess.getoutput(f"{CLI} sendtoaddress {address} {amount}")
-    return txid
+    return
 
 def get_utxo_index(address: str, txid: str) -> int:
     raw_tx = subprocess.getoutput(f"{CLI} getrawtransaction {txid}")
@@ -49,12 +45,11 @@ def get_utxo_index(address: str, txid: str) -> int:
         raise Exception(f"couldn't find output with address {address}")
     return index
 
-def create_regtest_utxo(address: str, amount: float) -> tuple([str, int]):
-    mining_address = setup_regtest_bitcoind()
-    txid = fund_address(address, amount)
+def fund_address(address: str, amount: float) -> (str, int):
+    txid = subprocess.getoutput(f"{CLI} sendtoaddress {address} {amount}")
     index = get_utxo_index(address, txid)
 
     # Mine a block to confirm the tx
-    subprocess.getoutput(f"{CLI} generatetoaddress 1 {mining_address}")
+    subprocess.getoutput(f"{CLI} generate 1")
 
-    return (txid, index)
+    return txid, index
