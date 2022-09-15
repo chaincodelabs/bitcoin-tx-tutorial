fd0a0b56e3f47f23685d623c70875d510df85600
Darius Parvin <darius@berkeley.edu>
2022-09-14 22:53:16 -0700

    add bip 0340 reference python functions

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,3 +2,4 @@ from functions.addresses import *
 from functions.create_regtest_utxo import *
 from functions.hashfunctions import *
 from functions.script import *
+import functions.bip_0340_reference as tr
